c83326a040bd34c834231bfe1c6e6766bff3f9b2
HsuMinHtet <hsumin.hmh@gmail.com>
2024-11-19 22:41:36 -0600

    commit for Login,main, /app,/features

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,60 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Header from './components/layout/header/header.jsx';
-import Footer from './components/layout/footer/footer.jsx';
-import Home from './pages/home/home.jsx';
-import Login from './pages/login/login.jsx';
-import Register from './pages/register/register.jsx';
-import './main.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux"; // Import Provider
+import { store, persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Header from "./components/layout/header/header.jsx";
+import Footer from "./components/layout/footer/footer.jsx";
+import Home from "./pages/home/home.jsx";
+import Login from "./pages/login/login.jsx";
+import Register from "./pages/register/register.jsx";
+import "./main.css";
 
 const applyDefaultTheme = () => {
-  const savedMode = localStorage.getItem('darkMode');
+  const savedMode = localStorage.getItem("darkMode");
 
   if (savedMode) {
-    if (savedMode === 'enabled') {
-      document.documentElement.classList.add('dark');
+    if (savedMode === "enabled") {
+      document.documentElement.classList.add("dark");
     }
   } else {
-    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const systemPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
     if (systemPrefersDark) {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('darkMode', 'enabled');
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("darkMode", "enabled");
     }
   }
 };
 
 applyDefaultTheme();
 
-createRoot(document.getElementById('budgetApp')).render(
+createRoot(document.getElementById("budgetApp")).render(
   <StrictMode>
-    
-    <Router>
-      
-      <Header />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Header />
 
-      <main className='h-85v bg-bgColor dark:bg-darkBgColor text-textColor dark:text-darkTextColor px-4 flex justify-center'>
+          <main className="h-85v bg-bgColor dark:bg-darkBgColor text-textColor dark:text-darkTextColor px-4 flex justify-center">
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </main>
 
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-
-      </main>
-
-      <Footer />
-
-    </Router>
-
-  </StrictMode>,
-)
+          <Footer />
+        </Router>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
