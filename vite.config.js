e156be9f4349d5e08c33471c7f47713ea36a7798
HsuMinHtet <hsumin.hmh@gmail.com>
2024-12-06 01:39:48 -0600

    base url

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Set the base URL for the app
+  base: "/", // Ensure this is set to the root
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: "./index.html", // Explicitly specify the entry point
+        main: "./index.html", // Entry point for Vite build
       },
     },
   },
